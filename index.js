137779b4fc3ab0015809e94cc8250fd0764b6954
Marcos Francesquini <francesquini.personal@gmail.com>
2023-06-19 21:49:57 +0200

    even arra and comparing

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   let mainArray = [];
   let oddArray = [];
+  let evenArray = [];
   let consecutiveOddCount = 0;
+  let consecutiveEvenCount = 0;
+  let isPreviousNumberOdd = false;
+  let isPreviousNumberEven = false;
 
   // Obtém uma referência para o campo de entrada e o botão de envio
   const inputNumber = document.getElementById("inputNumber");
   const submitButton = document.getElementById("submitButton");
   const showArray = document.querySelector("#showArray p");
   const showOddArray = document.querySelector("#showOddArray p");
+  const showEvenArray = document.querySelector("#showEvenArray p");
+  const showOddSum = document.getElementById("oddSumValue");
+  const showEvenSum = document.getElementById("evenSumValue");
 
   // Adiciona um ouvinte de evento para o clique no botão de envio
   submitButton.addEventListener("click", () => {
@@ -23,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Função para enviar o número digitado
   function submitNumber() {
+
     // Obtém o valor digitado no campo de entrada
     const numberValue = inputNumber.value;
     
@@ -34,11 +42,30 @@ document.addEventListener("DOMContentLoaded", () => {
         mainArray.unshift(number);
 
         if (number % 2 === 1) {
-          consecutiveOddCount++;
+          if (isPreviousNumberOdd) {
+            consecutiveOddCount++;
+          } else {
+            isPreviousNumberOdd = true;
+            consecutiveOddCount = 1;
+          }
         } else {
-          if (consecutiveOddCount > 0) {
+          if (isPreviousNumberOdd) {
+            isPreviousNumberOdd = false;     
             oddArray.unshift(consecutiveOddCount);
-            consecutiveOddCount = 0;
+          }
+        }
+
+        if (number % 2 === 0) {
+          if (isPreviousNumberEven) {
+            consecutiveEvenCount++;
+          } else {
+            isPreviousNumberEven = true;
+            consecutiveEvenCount = 1;
+          }
+        } else {
+          if (isPreviousNumberEven) {
+            isPreviousNumberEven = false;
+            evenArray.unshift(consecutiveEvenCount);
           }
         }
 
@@ -46,13 +73,20 @@ document.addEventListener("DOMContentLoaded", () => {
         inputNumber.value = "";
 
         // Exibe os valores digitados com um espaço entre eles
-        showArray.textContent = mainArray.join("|");
+        showArray.textContent = numberValue + " | " + showArray.textContent;
+
+        // Exibe a array dos números ímpares
         showOddArray.textContent = oddArray.join(", ");
+
+        // Exibe a array dos números pares
+        showEvenArray.textContent = evenArray.join(", ");
       }
     } else {
       inputNumber.value = "";
       alert("Digite um número entre 0 e 36.");
     }
+    showOddSum.textContent = consecutiveOddCount;
+    showEvenSum.textContent = consecutiveEvenCount;
   }
 });
 
